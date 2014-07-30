57b638717a2d90842a5b33bb0ff0b9a9d8cfd703
MoisesM <moises_1_2_3@hotmail.com>
2014-07-30 00:47:21 -0430

    Agregados nuevos codigos de error en "login.php" Notas: 	- Limpiado y formateado un poco mas, para mejor entendimiento 	  en la lectura del código

--- json/login.php
+++ json/login.php
@@ -2,8 +2,9 @@
 /* Códigos:
     0 = Usuario o clave inválida en el inicio de sesion
     1 = Se ha iniciado la sesión del usuario correctamente
+    2 = Error consultando en la base de datos
 */
-$msg = NULL;
+$msg['flag'] = 2;
 
 require_once('../config.php');
 $conexion = pg_connect('host='.$app['db']['host'].' port='.$app['db']['port'].' dbname='.$app['db']['name'].' user='.$app['db']['user'].' password='.$app['db']['pass']) OR die('Error de conexión con la base de datos');
@@ -12,16 +13,20 @@ $query = 'SELECT id, nombre_usuario, tipo_usuario FROM usuario WHERE nombre_usua
 
 if($login_query = pg_query($query)){
     $resultado = pg_fetch_assoc($login_query);
+    
     if(!empty($resultado['id'])){
         session_start();
         $_SESSION[strtolower(str_replace(' ', '_', $resultado['tipo_usuario']))] = $resultado['id'];
         $_SESSION['nombre_usuario'] = $resultado['nombre_usuario'];
         $msg['msg'] = '/pages';
         $msg['flag'] = 1;
-    } else{
+        
+    } else {
         $msg['msg'] = 'Usuario o clave inválida';
         $msg['flag'] = 0;
     }
+} else {
+    $msg['flag'] = 2;
 }
 pg_close($conexion);
 
