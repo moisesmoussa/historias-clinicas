5b26f7e20ca97413078ad73b2de142b9d97c6b6b
MoisesM <moises_1_2_3@hotmail.com>
2014-07-29 22:05:59 -0430

    Control de las rutas a las que pueden acceder los usuarios

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if($app['controller'] == 'usuarios' && $app['action'] == 'logout') {
 	header('Location: '.$app['basedir'].'/');
 }
 
-if(isset($_SESSION['general']) && $app['controller'] == 'usuarios' && $app['action'] != 'perfil' && $app['action'] != 'cambiar-clave')
+if(((isset($_SESSION['super_administrador']) || isset($_SESSION['administrador'])) && $app['controller'] == 'usuarios' && $app['action'] == 'login') || (isset($_SESSION['general']) && $app['controller'] == 'usuarios' && $app['action'] != 'perfil' && $app['action'] != 'cambiar-clave'))
     header('Location: '.$app['basedir'].'/pages');
 
 if(!isset($_SESSION['super_administrador']) && !isset($_SESSION['administrador']) && !isset($_SESSION['general'])){
