da431a3c8c1f806dd09cca681fb72c23ee6e4c31
MoisesM <moises_1_2_3@hotmail.com>
2014-08-01 16:41:12 -0430

    Corrección de control de códigos en "login.php" Notas: 	- Agregada verificación de campos vacíos y control del código 	  de error correspondiente 	- Agregados Mensajes de error faltantes de acuerdo al código 	  correspondiente 	- Modificada asignación de códigos de error 	- Modificados los comentarios

--- json/login.php
+++ json/login.php
@@ -1,34 +1,47 @@
 <?php
 /* Códigos:
-    0 = Usuario o clave inválida en el inicio de sesion
+    0 = Algún campo está vacío
     1 = Se ha iniciado la sesión del usuario correctamente
-    2 = Error consultando en la base de datos
+    2 = Usuario o clave inválida en el inicio de sesion
+    3 = Error consultando en la base de datos
 */
-$msg['flag'] = 2;
+$flag = 1;
 
-require_once('../config.php');
-$conexion = pg_connect('host='.$app['db']['host'].' port='.$app['db']['port'].' dbname='.$app['db']['name'].' user='.$app['db']['user'].' password='.$app['db']['pass']) OR die('Error de conexión con la base de datos');
-
-$query = 'SELECT id, nombre_usuario, tipo_usuario FROM usuario WHERE nombre_usuario = \''.$_POST['nombre'].'\' AND clave = \''.md5($_POST['clave']).'\'';
-
-if($login_query = pg_query($query)){
-    $resultado = pg_fetch_assoc($login_query);
-    
-    if(!empty($resultado['id'])){
-        session_start();
-        $_SESSION[strtolower(str_replace(' ', '_', $resultado['tipo_usuario']))] = $resultado['id'];
-        $_SESSION['nombre_usuario'] = $resultado['nombre_usuario'];
-        $msg['msg'] = '/pages';
-        $msg['flag'] = 1;
-        
-    } else {
-        $msg['msg'] = 'Usuario o clave inválida';
-        $msg['flag'] = 0;
+foreach ($_POST as $valor)
+    if(!isset($valor) || empty($valor)){
+        $flag = 0;
+        break;
     }
-} else {
-    $msg['flag'] = 2;
-}
-pg_close($conexion);
 
+if($flag){
+    require_once('../config.php');
+    $conexion = pg_connect('host='.$app['db']['host'].' port='.$app['db']['port'].' dbname='.$app['db']['name'].' user='.$app['db']['user'].' password='.$app['db']['pass']) OR die('Error de conexión con la base de datos');
+
+    $query = 'SELECT id, nombre_usuario, tipo_usuario FROM usuario WHERE nombre_usuario = \''.$_POST['nombre'].'\' AND clave = \''.md5($_POST['clave']).'\'';
+
+    if($login_query = pg_query($query)){
+        $resultado = pg_fetch_assoc($login_query);
+
+        if(!empty($resultado['id'])){
+            session_start();
+            $_SESSION[strtolower(str_replace(' ', '_', $resultado['tipo_usuario']))] = $resultado['id'];
+            $_SESSION['nombre_usuario'] = $resultado['nombre_usuario'];
+            $msg['msg'] = '/pages';
+            $msg['flag'] = 1;
+
+        } else {
+            $msg['msg'] = 'Usuario o clave inválida';
+            $msg['flag'] = 2;
+        }
+    } else {
+        $msg['msg'] = 'Error consultado en la base de datos';
+        $msg['flag'] = 3;
+    }
+    pg_close($conexion);
+    
+} else {
+    $msg['msg'] = 'Debe llenar todos los campos';
+    $msg['flag'] = 0;
+}
 echo json_encode($msg);
 ?>
