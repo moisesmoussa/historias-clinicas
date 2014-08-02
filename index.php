1d3c2ba362b1e49ee4c9b243fcb218ebb4af4851
MoisesM <moises_1_2_3@hotmail.com>
2014-08-02 16:42:52 -0430

    Corregido redireccionamiento a pág. principal Notas: 	- Modificados == por === 	- No es igual comparar == '' que comparar === '' el primero 	  es el que puede dar resultado true o false

--- index.php
+++ index.php
@@ -14,19 +14,19 @@ if(isset($_GET['params'])) {
 			unset($app['params'][$k]);
 }
 
-if($app['controller'] == 'usuarios' && $app['action'] == 'logout') {
+if($app['controller'] === 'usuarios' && $app['action'] === 'logout') {
 	foreach($_SESSION as $k => $v)
 		unset($_SESSION[$k]);
 
 	header('Location: '.$app['basedir'].'/');
 }
 
-if(((isset($_SESSION['super_administrador']) || isset($_SESSION['administrador'])) && $app['controller'] == 'usuarios' && $app['action'] == 'login') || (isset($_SESSION['general']) && $app['controller'] == 'usuarios' && $app['action'] != 'perfil' && $app['action'] != 'cambiar-clave'))
+if(((isset($_SESSION['super_administrador']) || isset($_SESSION['administrador'])) && ($app['controller'] === 'usuarios' && $app['action'] === 'login') || $app['controller'] == '') || (isset($_SESSION['general']) && $app['controller'] === 'usuarios' && $app['action'] != 'perfil' && $app['action'] != 'cambiar-clave'))
     header('Location: '.$app['basedir'].'/pages');
 
 if(!isset($_SESSION['super_administrador']) && !isset($_SESSION['administrador']) && !isset($_SESSION['general'])){
     require_once('modulos/usuarios/login.php');
-} else{
+} else {
 	require_once('layouts/default.php');
 }
 ?>
