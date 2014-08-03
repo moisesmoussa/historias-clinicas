9546202a444b0819158c3b837570784e12794592
MoisesM <moises_1_2_3@hotmail.com>
2014-08-03 17:30:39 -0430

    Agregados nuevos codigos de error Notas: 	- Agregado código de error para controlar unicidad del documento 	  de identidad 	- Agregado codigo de error para controlar error de consulta 	  en la base de datos 	- Limpieza de código

--- json/paciente/insertar/datos_paciente.php
+++ json/paciente/insertar/datos_paciente.php
@@ -4,10 +4,12 @@
     0 = Algún campo está vacío
     1 = Datos personales del paciente insertados correctamente en la BD
     2 = Los datos personales del paciente no se pudieron insertar en la BD
-    3 = No posee permisos para realizar la operación
+    3 = No se puede insertar en la BD porque el documento de identidad indicado del paciente ya existe y debe ser único
+    4 = Error consultando en la base de datos
+    5 = No posee permisos para realizar la operación
 */
 session_start();
-$msg['codigo'] = 3;
+$msg['codigo'] = 5;
 
 if(isset($_SESSION['super_administrador']) || isset($_SESSION['administrador']) || isset($_SESSION['general'])) {
     $flag = 1;
@@ -28,37 +30,51 @@ if(isset($_SESSION['super_administrador']) || isset($_SESSION['administrador']) 
         require_once('../../../config.php');
         $conexion = pg_connect('host='.$app['db']['host'].' port='.$app['db']['port'].' dbname='.$app['db']['name'].' user='.$app['db']['user'].' password='.$app['db']['pass']) OR die('Error de conexión con la base de datos');
 
-        if(isset($_SESSION['super_administrador']))
-            $id_usuario = $_SESSION['super_administrador'];
-        else if(isset($_SESSION['administrador']))
-            $id_usuario = $_SESSION['administrador'];
-        else if(isset($_SESSION['general']))
-            $id_usuario = $_SESSION['general'];
-
-        date_default_timezone_set('Etc/GMT+4');
-        $columnas = 'INSERT INTO paciente (fecha_ua, usuario_ua, creador, ';
-        $valores = 'VALUES (\''.date('Y-m-d').'\', '.$id_usuario.', '.$id_usuario.', ';
-        $len = count($_POST);
-        $cont = 0;
-
-        foreach ($_POST as $clave => $valor){
-            if($cont === $len - 1){
-                $columnas .= $clave.') ';
-                $valores .= '\''.$valor.'\') RETURNING id;';
+        $select = 'SELECT documento_identidad FROM paciente WHERE documento_identidad = \''.$_POST['documento_identidad'].'\'';
                 
-            } else {
-                $columnas .= $clave.',';
-                $valores .= '\''.$valor.'\',';
-            }
-            $cont++;
-        }
-        $query = $columnas . $valores;
+        if($query = pg_query($select)){
+            $respuesta = pg_fetch_assoc($query);
+
+            if(empty($respuesta['documento_identidad'])){
         
-        if($resultado = pg_query($query)) {
-            $msg['codigo'] = 1;
-            $msg['id'] = pg_fetch_assoc($resultado)['id'];
+                if(isset($_SESSION['super_administrador']))
+                    $id_usuario = $_SESSION['super_administrador'];
+                else if(isset($_SESSION['administrador']))
+                    $id_usuario = $_SESSION['administrador'];
+                else if(isset($_SESSION['general']))
+                    $id_usuario = $_SESSION['general'];
+
+                date_default_timezone_set('Etc/GMT+4');
+                $columnas = 'INSERT INTO paciente (fecha_ua, usuario_ua, creador, ';
+                $valores = 'VALUES (\''.date('Y-m-d').'\', '.$id_usuario.', '.$id_usuario.', ';
+                $len = count($_POST);
+                $cont = 0;
+
+                foreach ($_POST as $clave => $valor){
+                    if($cont === $len - 1){
+                        $columnas .= $clave.') ';
+                        $valores .= '\''.$valor.'\') RETURNING id;';
+
+                    } else {
+                        $columnas .= $clave.',';
+                        $valores .= '\''.$valor.'\',';
+                    }
+                    $cont++;
+                }
+                $query = $columnas . $valores;
+
+                if($resultado = pg_query($query)) {
+                    $msg['codigo'] = 1;
+                    $msg['id'] = pg_fetch_assoc($resultado)['id'];
+                    
+                } else {
+                    $msg['codigo'] = 2;
+                }
+            } else {
+                $msg['codigo'] = 3;
+            }
         } else {
-            $msg['codigo'] = 2;
+            $msg['codigo'] = 4;
         }
         pg_close($conexion);
         
