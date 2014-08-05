5fcaae02bdf7d130350eb1524758916463419e73
MoisesM <moises_1_2_3@hotmail.com>
2014-08-05 14:32:36 -0430

    Modificada página de bienvenida Notas: 	- Modificado tag "div" por un "section" 	- Modificado título de bienvenida 	- Modificado cuerpo de texto del mensaje de bienvenida 	- Modificado estilo de alineación a texto justificado

--- modulos/pages/index.php
+++ modulos/pages/index.php
@@ -1,12 +1,13 @@
-<div class="area-frontal">
+<section class="area-frontal">
     <h1 align="center">
-        <b>Bienvenido a tu perfil</b>
+        <b>Bienvenido a Historias Clínicas – FUNDAHOG!</b>
     </h1>
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
 
-    <p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
-
-    <p>Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh.</p>
-
-    <p>Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut</p>
-</div>
+	<p>La Fundación Hemato-Oncológica Guayana los invita a ingresar al Sistema de Historias Clínicas, el cual nos permite llevar el control y la gestión relativa a nuestros Pacientes, afianzando cada día nuestro compromiso sostenido, de ser una Organización sin fines de lucro de carácter apolítico y sin distinciones de raza o credo, destinada a la asistencia del enfermo con cáncer.</p>
+    
+	<p>Esta plataforma tecnológica constituye la base de datos que provee la información relativa a nuestros Pacientes, potenciando a través de esta importante herramienta,  nuestra MISION de Prestar orientación a los enfermos con cáncer, en cuanto a servicios médicos asistenciales y atención psicológica.</p>
+    
+	<p>El propósito sostenido de crecimiento de nuestra Fundación, nos ha permitido incorporar este Sistema de Historias Clínicas, para continuar construyendo y fortaleciendo el “Batallón de Ayuda” del paciente oncológico, para asistir a través de nuestros programas sociales a cualquier enfermo que acuda a nuestra Fundación, la cual permanece prestando constante asistencia desde su creación el 24 de Noviembre del Año 2004.</p>
+    
+	<p align="center"><b>Gracias por formar parte de este importante Equipo de Trabajo!</b></p>
+</section>
