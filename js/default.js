564151910a267c3f8de99cbf22582ec7c617b8a6
Mdsse <moises_1_2_3@hotmail.com>
2014-08-10 03:38:16 -0430

    Agregado archivo "default.js" Notas: 	- Agregado para controlar el scroll de la páguina y cambiar 	  la barra menu a un estilo "position: fixed" para que se mueva 	  junto con la página y poder acceder a él en todo momento

---
+++ js/default.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $e = $('.menu');
+
+        if ($(this).scrollTop() > 0 && $e.css('position') != 'fixed'){
+            $e.css({'position': 'fixed', 'top': '0px', 'width': '100%'});
+        }
+        if ($(this).scrollTop() < 1 && $e.css('position') === 'fixed'){
+            $e.css({'position': 'relative', 'top': 'auto', 'width': 'auto'});
+        }
+    });
+});
