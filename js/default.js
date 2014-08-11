982512b8e8bc0785aa65588a02286f4ee3f7ce59
Mdsse <moises_1_2_3@hotmail.com>
2014-08-11 00:34:24 -0430

    Limpieza de codigo con "beautify"

--- js/default.js
+++ js/default.js
@@ -1,12 +1,20 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         $e = $('.menu');
 
-        if ($(this).scrollTop() > 0 && $e.css('position') != 'fixed'){
-            $e.css({'position': 'fixed', 'top': '0px', 'width': '100%'});
+        if ($(this).scrollTop() > 0 && $e.css('position') != 'fixed') {
+            $e.css({
+                'position': 'fixed',
+                'top': '0px',
+                'width': '100%'
+            });
         }
-        if ($(this).scrollTop() < 1 && $e.css('position') === 'fixed'){
-            $e.css({'position': 'relative', 'top': 'auto', 'width': 'auto'});
+        if ($(this).scrollTop() < 1 && $e.css('position') === 'fixed') {
+            $e.css({
+                'position': 'relative',
+                'top': 'auto',
+                'width': 'auto'
+            });
         }
     });
 });
