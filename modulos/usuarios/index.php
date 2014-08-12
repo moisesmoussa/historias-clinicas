c3fcf316bc7299568c869bff1b19fe2e944eddfc
Mdsse <moises_1_2_3@hotmail.com>
2014-08-12 17:56:42 -0430

    Agregada barra de búsqueda de usuarios

--- modulos/usuarios/index.php
+++ modulos/usuarios/index.php
@@ -2,8 +2,11 @@
     <h1 align="center">Usuarios</h1>
     <p>
         <a class="enlace-registrar" href="<?php echo $app['basedir'].'/usuarios/registrar';?>">
-            <i class="fa fa-plus fa-fw"></i>    Registrar Usuario
+            <i class="fa fa-plus fa-fw"></i> Registrar Usuario
         </a>
+        <br>
+        <input type="text" class="input-buscar" placeholder="Buscar Usuarios" /><i class="fa fa-search fa-fw icono-buscar"></i>
     </p>
+    <br>
     <table class="usuarios"></table>
 </section>
