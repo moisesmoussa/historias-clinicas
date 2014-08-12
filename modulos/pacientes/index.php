0fbc41696dbddc6f7ec89db4aa83ebb2f157f9ea
Mdsse <moises_1_2_3@hotmail.com>
2014-08-12 17:56:34 -0430

    Agregada barra de búsqueda de pacientes

--- modulos/pacientes/index.php
+++ modulos/pacientes/index.php
@@ -2,8 +2,11 @@
     <h1 align="center">Pacientes</h1>
     <p>
         <a class="enlace-registrar" href="<?php echo $app['basedir'].'/pacientes/registrar';?>">
-            <i class="fa fa-plus fa-fw"></i>    Registrar Paciente
+            <i class="fa fa-plus fa-fw"></i> Registrar Paciente
         </a>
+        <br>
+        <input type="text" class="input-buscar" placeholder="Buscar Pacientes" /><i class="fa fa-search fa-fw icono-buscar"></i>
     </p>
+    <br>
     <table class="pacientes"></table>
 </section>
