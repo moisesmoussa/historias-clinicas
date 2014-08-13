17b379602837b4c5d9dbc92b3b5bce2738b1c644
Mdsse <moises_1_2_3@hotmail.com>
2014-08-13 18:39:19 -0430

    Modificada eliminación de usuarios y pacientes Notas: 	- Modificada para poder eliminar uno o varios usuarios y pacientes

--- json/paciente/eliminar.php
+++ json/paciente/eliminar.php
@@ -1,25 +1,41 @@
 <?php
 /* Códigos:
-    0 = No se pudo eliminar el paciente indicado en la BD
-    1 = Paciente eliminado correctamente en la BD
-    2 = No posee permisos para realizar la operación
+    0 = No se pudo eliminar el o los paciente(s) indicado(s) en la BD
+    1 = Paciente(s) eliminado(s) correctamente en la BD
+    2 = No posee permisos para eliminar pacientes
 */
 session_start();
-$msg['msg'] = 'No posee permisos para eliminar el paciente';
+$msg['msg'] = 'No posee permisos para eliminar pacientes';
 $msg['flag'] = 2;
 
 if(isset($_SESSION['super_administrador']) || isset($_SESSION['administrador'])) {
     require_once('../../config.php');
     $conexion = pg_connect('host='.$app['db']['host'].' port='.$app['db']['port'].' dbname='.$app['db']['name'].' user='.$app['db']['user'].' password='.$app['db']['pass']) OR die('Error de conexión con la base de datos');
 
-    $query = 'DELETE FROM paciente WHERE id = '.$_POST['paciente'];
+    $cont = 0;
+    $flag = true;
     
-    if(pg_query($query)){
-        $msg['msg'] = 'Paciente eliminado exitosamente';
+    foreach ($_POST['paciente'] as $valor)
+        $query[$cont++] = 'DELETE FROM paciente WHERE id = '.$valor;
+    
+    foreach($query as $valor)
+        if(!pg_query($valor))
+            $flag = false;
+        
+    if($flag){
+        $msg['msg'] = (count($_POST['paciente']) === 1) ? 'Paciente eliminado exitosamente' : 'Pacientes indicados eliminados exitosamente';
         $msg['flag'] = 1;
+            
     } else {
-        $msg['msg'] = 'Error con la base de datos, no se pudo eliminar el paciente';
-        $msg['flag'] = 0;
+        $msg['msg'] = 'Error con la base de datos, ';
+        
+        if(count($_POST['paciente']) === 1){
+            $msg['msg'] .= 'no se pudo eliminar el paciente';
+            $msg['flag'] = 0;
+        } else {
+            $msg['msg'] .= 'no se pudieron eliminar alguno(s) o todos los pacientes indicados, por favor verifique';
+            $msg['flag'] = 1;
+        }
     }
     pg_close($conexion);
 }
--- json/usuario/eliminar.php
+++ json/usuario/eliminar.php
@@ -1,25 +1,41 @@
 <?php
 /* Códigos:
-    0 = No se pudo eliminar el usuario indicado en la BD
-    1 = Usuario eliminado correctamente en la BD
-    2 = No posee permisos para realizar la operación
+    0 = No se pudo eliminar el o los usuario(s) indicado(s) en la BD
+    1 = Usuario(s) eliminado(s) correctamente en la BD
+    2 = No posee permisos para eliminar usuarios
 */
 session_start();
-$msg['msg'] = 'No posee permisos para eliminar el usuario';
+$msg['msg'] = 'No posee permisos para eliminar usuarios';
 $msg['flag'] = 2;
 
 if(isset($_SESSION['super_administrador']) || isset($_SESSION['administrador'])) {
     require_once('../../config.php');
     $conexion = pg_connect('host='.$app['db']['host'].' port='.$app['db']['port'].' dbname='.$app['db']['name'].' user='.$app['db']['user'].' password='.$app['db']['pass']) OR die('Error de conexión con la base de datos');
 
-    $query = 'DELETE FROM usuario WHERE id = '.$_POST['usuario'];
+    $cont = 0;
+    $flag = true;
     
-    if(pg_query($query)){
-        $msg['msg'] = 'Usuario eliminado exitosamente';
+    foreach ($_POST['usuario'] as $valor)
+        $query[$cont++] = 'DELETE FROM usuario WHERE id = '.$valor;
+    
+    foreach($query as $valor)
+        if(!pg_query($valor))
+            $flag = false;
+        
+    if($flag){
+        $msg['msg'] = (count($_POST['usuario']) === 1) ? 'Usuario eliminado exitosamente' : 'Usuarios indicados eliminados exitosamente';
         $msg['flag'] = 1;
+            
     } else {
-        $msg['msg'] = 'Error con la base de datos, no se pudo eliminar el usuario';
-        $msg['flag'] = 0;
+        $msg['msg'] = 'Error con la base de datos, ';
+        
+        if(count($_POST['usuario']) === 1){
+            $msg['msg'] .= 'no se pudo eliminar el usuario';
+            $msg['flag'] = 0;
+        } else {
+            $msg['msg'] .= 'no se pudieron eliminar alguno(s) o todos los usuarios indicados, por favor verifique';
+            $msg['flag'] = 1;
+        }
     }
     pg_close($conexion);
 }
