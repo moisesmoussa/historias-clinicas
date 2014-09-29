9dcc66e011f6ef65bb25ea818cc82b61913b2520
Mdsse <moises_1_2_3@hotmail.com>
2014-09-29 12:42:35 -0430

    Agregado order by por num historia clínica

--- json/paciente/buscar.php
+++ json/paciente/buscar.php
@@ -14,7 +14,7 @@ if(isset($_SESSION['super_administrador']) || isset($_SESSION['administrador']) 
     require_once('../../config.php');
     $conexion = pg_connect('host='.$app['db']['host'].' port='.$app['db']['port'].' dbname='.$app['db']['name'].' user='.$app['db']['user'].' password='.$app['db']['pass']) OR die('Error de conexión con la base de datos');
 
-    $select = 'SELECT id, nro_historia_clinica, documento_identidad, apellidos, nombres, tlf_movil, correo_electronico FROM paciente WHERE nro_historia_clinica LIKE \'%'.$_POST['busqueda'].'%\' OR documento_identidad LIKE \'%'.$_POST['busqueda'].'%\' OR nombres LIKE \'%'.$_POST['busqueda'].'%\' OR apellidos LIKE \'%'.$_POST['busqueda'].'%\' OR tlf_movil LIKE \'%'.$_POST['busqueda'].'%\' OR correo_electronico LIKE \'%'.$_POST['busqueda'].'%\'';
+    $select = 'SELECT id, nro_historia_clinica, documento_identidad, apellidos, nombres, tlf_movil, correo_electronico FROM paciente WHERE nro_historia_clinica LIKE \'%'.$_POST['busqueda'].'%\' OR documento_identidad LIKE \'%'.$_POST['busqueda'].'%\' OR nombres LIKE \'%'.$_POST['busqueda'].'%\' OR apellidos LIKE \'%'.$_POST['busqueda'].'%\' OR tlf_movil LIKE \'%'.$_POST['busqueda'].'%\' OR correo_electronico LIKE \'%'.$_POST['busqueda'].'%\' ORDER BY nro_historia_clinica::int';
     
     if($query = pg_query($select)){        
         $cont = 0;
--- json/paciente/cargar_todos.php
+++ json/paciente/cargar_todos.php
@@ -13,7 +13,7 @@ if(isset($_SESSION['super_administrador']) || isset($_SESSION['administrador']) 
     require_once('../../config.php');
     $conexion = pg_connect('host='.$app['db']['host'].' port='.$app['db']['port'].' dbname='.$app['db']['name'].' user='.$app['db']['user'].' password='.$app['db']['pass']) OR die('Error de conexión con la base de datos');
 
-    $select = 'SELECT id, nro_historia_clinica, documento_identidad, apellidos, nombres, tlf_movil, correo_electronico FROM paciente';
+    $select = 'SELECT id, nro_historia_clinica, documento_identidad, apellidos, nombres, tlf_movil, correo_electronico FROM paciente ORDER BY nro_historia_clinica::int';
     
     if($query = pg_query($select)){
         unset($msg['msg']);
