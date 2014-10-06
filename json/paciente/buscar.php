04c34fbe8572891cbf0d107deb24d962242421cd
Mdsse <moises_1_2_3@hotmail.com>
2014-10-05 20:17:06 -0430

    Modificada búsqueda de pacientes Notas: 	- Agregada verificación de campo de ordenamiento para ordenar los resultados 	  por dicho campo indicado

--- json/paciente/buscar.php
+++ json/paciente/buscar.php
@@ -14,7 +14,32 @@ if(isset($_SESSION['super_administrador']) || isset($_SESSION['administrador']) 
     require_once('../../config.php');
     $conexion = pg_connect('host='.$app['db']['host'].' port='.$app['db']['port'].' dbname='.$app['db']['name'].' user='.$app['db']['user'].' password='.$app['db']['pass']) OR die('Error de conexión con la base de datos');
 
-    $select = 'SELECT id, nro_historia_clinica, documento_identidad, apellidos, nombres, tlf_movil, correo_electronico FROM paciente WHERE nro_historia_clinica LIKE \'%'.$_POST['busqueda'].'%\' OR documento_identidad LIKE \'%'.$_POST['busqueda'].'%\' OR nombres LIKE \'%'.$_POST['busqueda'].'%\' OR apellidos LIKE \'%'.$_POST['busqueda'].'%\' OR tlf_movil LIKE \'%'.$_POST['busqueda'].'%\' OR correo_electronico LIKE \'%'.$_POST['busqueda'].'%\' ORDER BY nro_historia_clinica::int';
+    if(!empty($order = $_POST['order'])) {
+        switch($order['field']) {
+            case 'Número de Historia Clínica':
+                $order = 'nro_historia_clinica::int ' . $order['type'];
+                break;
+            case 'Documento de Identidad':
+                $order = 'documento_identidad ' . $order['type'];
+                break;
+            case 'Nombres':
+                $order = 'nombres ' . $order['type'];
+                break;
+            case 'Apellidos':
+                $order = 'apellidos ' . $order['type'];
+                break;
+            case 'Móvil':
+                $order = 'tlf_movil ' . $order['type'];
+                break;
+            case 'Email':
+                $order = 'correo_electronico ' . $order['type'];
+                break;
+        }
+    } else {
+        $order = 'nro_historia_clinica::int';
+    }
+    
+    $select = 'SELECT id, nro_historia_clinica, documento_identidad, apellidos, nombres, tlf_movil, correo_electronico FROM paciente WHERE nro_historia_clinica LIKE \'%'.$_POST['busqueda'].'%\' OR documento_identidad LIKE \'%'.$_POST['busqueda'].'%\' OR nombres LIKE \'%'.$_POST['busqueda'].'%\' OR apellidos LIKE \'%'.$_POST['busqueda'].'%\' OR tlf_movil LIKE \'%'.$_POST['busqueda'].'%\' OR correo_electronico LIKE \'%'.$_POST['busqueda'].'%\' ORDER BY ' . $order;
     
     if($query = pg_query($select)){        
         $cont = 0;
