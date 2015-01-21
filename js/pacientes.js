a87e905f1cfd014c7034e069a486dae43852fc05
Mdsse <moises_1_2_3@hotmail.com>
2015-01-21 19:09:57 -0430

    Corección de correo vacío

--- js/pacientes.js
+++ js/pacientes.js
@@ -140,8 +140,12 @@ function cargarPacientes(busqueda, order) {
 
                 if (datos.flag === 1) {
                     for (var i in datos.paciente) {
-                        html += '<tr><td class="icono-tabla" data-id="' + datos.paciente[i].id + '"><i class="fa fa-trash-o fa-2x icon borrar" title="Eliminar paciente"></i><i class="fa fa-edit fa-2x icon editar" title="Modificar paciente"></i><i class="fa fa-medkit fa-2x icon diagnostico" title="Agregar y/o modificar diagnóstico del paciente"></i></td><td>' + datos.paciente[i].nro_historia_clinica + '</td><td>' + datos.paciente[i].documento_identidad + '</td><td>' + datos.paciente[i].nombres + '</td><td>' + datos.paciente[i].apellidos + '</td><td>' + datos.paciente[i].tlf_movil + '</td><td>' + datos.paciente[i].correo_electronico + '</td></tr>';
+                        html += '<tr><td class="icono-tabla" data-id="' + datos.paciente[i].id + '"><i class="fa fa-trash-o fa-2x icon borrar" title="Eliminar paciente"></i><i class="fa fa-edit fa-2x icon editar" title="Modificar paciente"></i><i class="fa fa-medkit fa-2x icon diagnostico" title="Agregar y/o modificar diagnóstico del paciente"></i></td><td>' + datos.paciente[i].nro_historia_clinica + '</td><td>' + datos.paciente[i].documento_identidad + '</td><td>' + datos.paciente[i].nombres + '</td><td>' + datos.paciente[i].apellidos + '</td><td>' + datos.paciente[i].tlf_movil + '</td><td>';
                     }
+                    if(datos.paciente[i].correo_electronico != 'null')
+                        html += datos.paciente[i].correo_electronico;
+                    
+                    html +=  '</td></tr>';
                     $('.pacientes').html(html);
 
                 } else {
