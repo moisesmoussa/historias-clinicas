ace5fe5b1ce206dfaf74aa6894d630493fa7405c
Mdsse <moises_1_2_3@hotmail.com>
2015-01-21 21:59:40 -0430

    Modificado mensaje de bienvenida al sistema

--- modulos/pages/index.php
+++ modulos/pages/index.php
@@ -1,13 +1,13 @@
 <section class="area-frontal">
     <h1 align="center">
-        <b>Bienvenido a Historias Clínicas – FUNDAHOG!</b>
+        <b>Bienvenido a Historias Clínicas – IVSS!</b>
     </h1>
 
-	<p>La Fundación Hemato-Oncológica Guayana los invita a ingresar al Sistema de Historias Clínicas, el cual nos permite llevar el control y la gestión relativa a nuestros Pacientes, afianzando cada día nuestro compromiso sostenido, de ser una Organización sin fines de lucro de carácter apolítico y sin distinciones de raza o credo, destinada a la asistencia del enfermo con cáncer.</p>
+	<p>El Instituto Venezolano de los Seguros Sociales los invita a ingresar al Sistema de Historias Clínicas, el cual nos permite llevar el control y la gestión relativa a nuestros Pacientes con cáncer, afianzando cada día nuestro compromiso sostenido como institución pública que colabora con la sociedad venezolana.</p>
     
-	<p>Esta plataforma tecnológica constituye la base de datos que provee la información relativa a nuestros Pacientes, potenciando a través de esta importante herramienta,  nuestra MISION de Prestar orientación a los enfermos con cáncer, en cuanto a servicios médicos asistenciales y atención psicológica.</p>
+	<p>Esta plataforma tecnológica constituye la base de datos que provee la información relativa a nuestros Pacientes con cáncer, potenciando a través de esta importante herramienta,  nuestra MISION de brindar protección de la Seguridad Social a todos los beneficiarios en las contingencias de maternidad, vejez, sobrevivencia, enfermedad, accidentes, incapacidad, invalidez, nupcias, muerte, retiro y cesantía o perdida de empleo, de manera oportuna y con calidad de excelencia en el servicio prestado, dentro del marco legal que lo regula.</p>
     
-	<p>El propósito sostenido de crecimiento de nuestra Fundación, nos ha permitido incorporar este Sistema de Historias Clínicas, para continuar construyendo y fortaleciendo el “Batallón de Ayuda” del paciente oncológico, para asistir a través de nuestros programas sociales a cualquier enfermo que acuda a nuestra Fundación, la cual permanece prestando constante asistencia desde su creación el 24 de Noviembre del Año 2004.</p>
+	<p>El Instituto Venezolano de los Seguros Sociales, bajo la inspiración de la justicia social y de la equidad para toda la población, avanza hacia la conformación de la nueva estructura de la sociedad, garantizando el cumplimiento de los principios y normas de la Seguridad Social a todos los habitantes del país desde la creación de esta institución el 9 de octubre de 1944. El compromiso social y el sentido de identificación con la labor que se realiza, debe ser la premisa fundamental en todos los servicios prestados.</p>
     
 	<p align="center"><b>Gracias por formar parte de este importante Equipo de Trabajo!</b></p>
 </section>
