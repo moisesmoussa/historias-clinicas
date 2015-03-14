6f4e021bb2b08d7f51813622ef7b58291262cf30
Mdsse <moises_1_2_3@hotmail.com>
2015-03-14 16:39:10 -0430

    Agregada navegación por páginas Notas: 	- Agregado campo tipo "select" denominado "ordenar por" para que 	  el usuario escoga la forma en que quiere ordenar los 	  resultados obtenidos 	- Agregado campo tipo "select" denominado "mostrar" para que 	  el usuario indique la cantidad de resultados que quiere 	  observar por página 	- Agregada barra de paginación para navegar por las páginas 	  obtenidas en los resultados encontrados 	- Agregado label que indica el número de resultados que se observan 	  por página y el total 	- Agregado envoltorio para las tablas para permitir que aparezca 	  un scroll bar cuando el tamaño de la página es menor que el de 	  la tabla de resultados 	- Agregada la misma barra de paginación pero en el fondo de la tabla 	  para la navegación del usuario cuando se encuentra al final y no 	  tenga que subir hasta el inicio para seguir moviéndose por las 	  páginas de resultados

--- modulos/usuarios/index.php
+++ modulos/usuarios/index.php
@@ -13,6 +13,40 @@
             <i class="fa fa-trash-o fa-fw"></i> Eliminar Seleccionados
         </label>
     </p>
+    <nav>
+        <label>Ordenar por</label>
+        <select class="order-by">
+            <option value="cedula-ASC">Cédula: Menor a mayor</option>
+            <option value="cedula-DESC">Cédula: Mayor a menor</option>
+            <option value="nombres-ASC">Nombre: A - Z</option>
+            <option value="nombres-DESC">Nombre: Z - A</option>
+            <option value="apellidos-ASC">Apellido: A - Z</option>
+            <option value="apellidos-DESC">Apellido: Z - A</option>
+        </select>
+        <label>Mostrar</label>
+        <select class="show">
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="75">75</option>
+            <option value="100">100</option>
+            <option value="ALL">Todos</option>
+        </select>
+        <label>por página</label>
+        <br>
+        <br>
+        <label class="showing"></label>
+        <ul class="pagination"></ul>
+    </nav>
     <br>
-    <table class="usuarios"></table>
+    <br>
+    <div class="table-wrapper">
+        <table class="usuarios"></table>
+    </div>
+    <br>
+    <nav class="bottom-pagination">
+        <label class="showing"></label>
+        <ul class="pagination"></ul>
+    </nav>
+    <br>
 </section>
