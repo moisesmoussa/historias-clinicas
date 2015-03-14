e1a6bc47456e277d7442106e2f62b4c003521f00
Mdsse <moises_1_2_3@hotmail.com>
2015-03-14 16:32:54 -0430

    Mejorada carga de pacientes para paginación Notas: 	- La carga de datos de los pacientes en el back-end ahora incluye la 	  funcionalidad necesaria para filtrar por páginas 	- Mejorada funcionalidad de ordenamiento de la lista de pacientes 	  para cargar

--- json/paciente/cargar_todos.php
+++ json/paciente/cargar_todos.php
@@ -14,34 +14,45 @@ if(isset($_SESSION['super_administrador']) || isset($_SESSION['administrador']) 
     $conexion = pg_connect('host='.$app['db']['host'].' port='.$app['db']['port'].' dbname='.$app['db']['name'].' user='.$app['db']['user'].' password='.$app['db']['pass']) OR die('Error de conexión con la base de datos');
 
     if(!empty($order = $_POST['order'])) {
-        switch($order['field']) {
-            case 'Número de Historia Clínica':
-                $order = 'nro_historia_clinica::int ' . $order['type'];
-                break;
-            case 'Documento de Identidad':
-                $order = 'documento_identidad ' . $order['type'];
-                break;
-            case 'Nombres':
-                $order = 'nombres ' . $order['type'];
-                break;
-            case 'Apellidos':
-                $order = 'apellidos ' . $order['type'];
-                break;
-            case 'Móvil':
-                $order = 'tlf_movil ' . $order['type'];
-                break;
-            case 'Email':
-                $order = 'correo_electronico ' . $order['type'];
-                break;
+        if($order['field'] === 'nro_historia_clinica' || $order['field'] === 'documento_identidad') {
+            $order['field'] .= '::int';
         }
+        $order = $order['field'] . ' ' . $order['type'];
+        
     } else {
-        $order = 'nro_historia_clinica::int';
+        $order = 'nro_historia_clinica::int ASC';
     }
     
-    $select = 'SELECT id, nro_historia_clinica, documento_identidad, apellidos, nombres, tlf_movil, correo_electronico FROM paciente ORDER BY ' . $order;
+    if(empty($show = $_POST['show']))
+        $show = '10';
+    
+    if(empty($offset = $_POST['offset']))
+        $offset = '0';
+    
+    $totalResults = 0;
+    $count = 'SELECT COUNT(*) FROM paciente';
+    $select = 'SELECT id, nro_historia_clinica, documento_identidad, apellidos, nombres, tlf_movil, correo_electronico FROM paciente ORDER BY ' . $order . ' LIMIT ' . $show . ' OFFSET ' . $offset;
+    
+    if($query = pg_query($count)){
+        $totalResults = pg_fetch_assoc($query)['count'];
+        
+        if($show !== 'ALL')
+            $msg['countPages'] = ceil($totalResults/$show);
+        else
+            $msg['countPages'] = 1;
+        
+    } else {
+        $msg['msg'] = 'Error de consulta en la base de datos';
+        $msg['flag'] = 0;
+        pg_close($conexion);
+        return json_encode($msg);
+    }
     
     if($query = pg_query($select)){
         unset($msg['msg']);
+        $msg['show'] = $show;
+        $msg['totalResults'] = (int) $totalResults;
+        $msg['offset'] = (int) $offset;
         $msg['flag'] = 1;
         $cont = 0;
         
