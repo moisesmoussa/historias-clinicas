2b1c9b65ef12bff4961d145ffc6f5877fa959bad
Mdsse <moises_1_2_3@hotmail.com>
2015-03-15 19:03:56 -0430

    Modificado redireccionamiento de rutas Notas: 	- La ruta para iniciar la sesión de un usuario ahora se realiza por medio de 	  el controller "usuarios" y la acción "login" 	- Se agregó el direccionamiento hacia un layout denominado "guest" 	  que se encarga de mostrar las vistas determinadas para usuarios 	  que no tienen sesión iniciada en el sistema 	- La ruta de inicio del sistema ahora se define con el controlador "usuarios" 	  y la acción "home"

--- index.php
+++ index.php
@@ -18,14 +18,19 @@ if($app['controller'] === 'usuarios' && $app['action'] === 'logout') {
 	foreach($_SESSION as $k => $v)
 		unset($_SESSION[$k]);
 
-	header('Location: '.$app['basedir'].'/');
+	header('Location: '.$app['basedir'].'/usuarios/login');
 }
 
-if( ((isset($_SESSION['super_administrador']) || isset($_SESSION['administrador'])) && ($app['controller'] === 'usuarios' && $app['action'] === 'login') || $app['controller'] == '') || (isset($_SESSION['general']) && $app['controller'] === 'usuarios' && $app['action'] != 'perfil' && $app['action'] != 'cambiar-clave') || ( ($app['action'] === 'modificar' || $app['action'] === 'diagnostico') && $app['params'] == '') )
-    header('Location: '.$app['basedir'].'/pages');
+$flagLogin = isset($_SESSION['super_administrador']) || isset($_SESSION['administrador']) || isset($_SESSION['general']);
+$flagNotLogin = !isset($_SESSION['super_administrador']) && !isset($_SESSION['administrador']) && !isset($_SESSION['general']);
 
-if(!isset($_SESSION['super_administrador']) && !isset($_SESSION['administrador']) && !isset($_SESSION['general'])){
-    require_once('modulos/usuarios/login.php');
+if( ((isset($_SESSION['super_administrador']) || isset($_SESSION['administrador'])) && (($app['controller'] === 'usuarios' && $app['action'] === 'login') || $app['controller'] == '')) || (isset($_SESSION['general']) && $app['controller'] === 'usuarios' && $app['action'] != 'perfil' && $app['action'] != 'cambiar-clave') || ($flagLogin && ($app['action'] === 'modificar' || $app['action'] === 'diagnostico') && $app['params'] == '') )
+    header('Location: '.$app['basedir'].'/usuarios/home');
+
+if($flagNotLogin && $app['controller'] === 'usuarios' && ($app['action'] === 'login' || $app['action'] === 'forgot-password')) {
+    require_once('layouts/guest.php');
+} else if($flagNotLogin){
+    header('Location: '.$app['basedir'].'/usuarios/login');
 } else {
 	require_once('layouts/default.php');
 }
