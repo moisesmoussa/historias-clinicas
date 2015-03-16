09824d923bb8f6b9bd02dd3586a38fc54fe67e73
Mdsse <moises_1_2_3@hotmail.com>
2015-03-16 04:03:09 -0430

    Simplicados campos del formulario Notas: 	- Simplificados los atributos de los campos del formulario para 	  reestablecimiento de contraseña

--- modulos/usuarios/password_reset.php
+++ modulos/usuarios/password_reset.php
@@ -1,25 +1,28 @@
 <section class="contenedor-formulario">
-    <h2 align="center">Cambiar Contraseña</h2>
-    <form id="nueva-clave" action="" autocomplete="off">
+    <h2 align="center">Reestablecer Contraseña</h2>
+    <form id="form-password-reset" action="" autocomplete="off">
         <table class="formulario">
             <tr>
+                <td class="oculto">
+                    <input name="token" type="text" value="<?php echo $token ?>">
+                </td>
                 <td>
-                    <label for="clave_nueva">Contraseña Nueva: <i class="fa fa-question-circle fa-fw ayuda" title="<b>Puede estar formado por:</b><br>Mínimo 6 caracteres<br>Máximo 18 caracteres<br>Letras mayúsculas y minúsculas<br>Números<br>Caracteres: * + / : . , $ % & # { } _ -"></i>
+                    <label for="new_password">Contraseña Nueva: <i class="fa fa-question-circle fa-fw ayuda" title="<b>Puede estar formado por:</b><br>Mínimo 6 caracteres<br>Máximo 18 caracteres<br>Letras mayúsculas y minúsculas<br>Números<br>Caracteres: * + / : . , $ % & # { } _ -"></i>
                     </label>
                     <br>
-                    <input id="clave_nueva" name="clave_nueva" type="password" pattern="^[a-zA-Z0-9\*\+\/\:\.\,\$\%\&\#\{\}_-]{6,18}$" autofocus required>
+                    <input name="new_password" type="password" pattern="^[a-zA-Z0-9\*\+\/\:\.\,\$\%\&\#\{\}_-]{6,18}$" autofocus required>
                 </td>
             </tr>
             <tr>
                 <td>
-                    <label for="clave_nueva2">Confirmar Contraseña:</label>
+                    <label for="password_confirmation">Confirmar Contraseña:</label>
                     <br>
-                    <input id="clave_nueva2" name="clave_nueva2" type="password" pattern="^[a-zA-Z0-9\*\+\/\:\.\,\$\%\&\#\{\}_-]{6,18}$" required>
+                    <input name="password_confirmation" type="password" pattern="^[a-zA-Z0-9\*\+\/\:\.\,\$\%\&\#\{\}_-]{6,18}$" required>
                 </td>
             </tr>
             <tr>
                 <td>
-                    <input type="submit" class="boton" value="Guardar Cambios" />
+                    <input type="submit" class="boton" value="Guardar Datos" />
                 </td>
             </tr>
             <tr>
